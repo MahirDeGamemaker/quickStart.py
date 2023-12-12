be21d512fe6df3fbf07fb1e242eb248871d09283
MahirDeGamemaker <150327418+MahirDeGamemaker@users.noreply.github.com>
2023-12-12 23:42:17 +0600

    Update and rename main.py to Quickstartpygame.py

--- Quickstartpygame.py
+++ Quickstartpygame.py
@@ -15,8 +15,13 @@ class game():
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((800, 600))
-        pygame.display.set_caption("In production")
+        pygame.display.set_caption("quickStart.py")
         self.display = pygame.Surface((400, 300))
+
+        #rectangle
+        self.rect_pos = pygame.Rect(50, 50, 50, 50)
+        self.rect_spd = 5
+
         self.clock = pygame.time.Clock()
     def run(self):
         while True:
@@ -25,13 +30,27 @@ class game():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-            
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
+
+                #exit event code
+                if event.type == pygame.KEYDOWN: 
+                       if event.key == pygame.K_ESCAPE:
                         pygame.quit()
                         sys.exit()
 
+            #getting the input
+            key = pygame.key.get_pressed()
+            if key[pygame.K_LEFT]or key[pygame.K_a]:
+                self.rect_pos.x -= self.rect_spd
+            if key[pygame.K_RIGHT]or key[pygame.K_d]:
+                self.rect_pos.x += self.rect_spd
+            if key[pygame.K_UP]or key[pygame.K_w]:
+                self.rect_pos.y -= self.rect_spd
+            if key[pygame.K_DOWN]or key[pygame.K_s]:
+                self.rect_pos.y += self.rect_spd
+            
+            #drawing stuff to the screen
             self.display.fill((255, 255, 255))
+            pygame.draw.rect(self.display, 'red', self.rect_pos)
 
             self.screen.blit(pygame.transform.scale2x(self.display), (0, 0))
             pygame.display.flip()
